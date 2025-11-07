24b1b180e07c16312a6b1043355bf9eb42efc172
AlexPF43 <alejandroperez.24@campuscamara.es>
2025-11-07 18:46:16 +0100

    Remove unused service routes
    Arreglado el server.js

--- proyectoNode/server.js
+++ proyectoNode/server.js
@@ -41,13 +41,6 @@ app.use('/api/pedidos', require('./routes/pedidosRoutes'));
 app.use('/api/carritos', require('./routes/carritosRoutes'));
 app.use('/api/proveedores', require('./routes/proveedoresRoutes'));
 
-app.use('/api/services/carritos', require('./services/carritosService.js'));
-app.use('/api/services/categorias', require('./services/categoriasService.js'));
-app.use('/api/services/clientes', require('./services/clientesService.js'));
-app.use('/api/services/pedidos', require('./services/pedidosService.js'));
-app.use('/api/services/productos', require('./services/productosService.js'));
-app.use('/api/services/proveedores', require('./services/proveedoresService.js'));
-
 // ======== Iniciar Servidor ========
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
